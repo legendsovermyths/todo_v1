5311c94c10d8c629ee63063e0ba38eadb83808a7
Mask Wolf <maskwolf2000@gmail.com>
2020-07-10 02:40:08 +0530

    add css

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const app=express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"))
+var items=[]
 app.listen(3000,function(req,res){
 
   console.log("server is up and running on port 3000");
 })
 app.get("/",function(req,res){
-    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var options = { weekday: 'long', month: 'long', day: 'numeric' };
     var today  = new Date();
     var day=today.toLocaleDateString("hi-IN", options)
     console.log(day)
-    res.render("lists",{kindofday:day});
+    res.render("lists",{kindofday:day,newitem:items});
   }
 )
 app.post("/",function(req,res){
-  console.log("request recieved")
+  var item=req.body.itemName
+  items.push(item)
+  res.redirect("/")
 })
