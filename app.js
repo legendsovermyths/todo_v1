25e8bca71e16cd6af91672560565151f349d808d
Mask Wolf <maskwolf2000@gmail.com>
2020-07-10 01:41:17 +0530

    outline complete

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-const express=require("express")
-const bodyParser=require("body-parser")
-const app=express()
-app.set("view engine","ejs");
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+app.set('view engine', 'ejs');
 app.listen(3000,function(req,res){
-  console.log("server is up and running on port 3000")
+
+  console.log("server is up and running on port 3000");
 })
 app.get("/",function(req,res){
-  res.send("Server is running")
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var today  = new Date();
+    var day=today.toLocaleDateString("hi-IN", options)
+    console.log(day)
+    res.render("lists",{kindofday:day});
+  }
+)
+app.post("/",function(req,res){
+  console.log("request recieved")
 })
